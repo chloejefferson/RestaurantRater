1e5657c2706c7e55720c030ffe6fa574dcc18297
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-03-30 18:18:12 -0400

    Create functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -16,5 +16,25 @@ namespace RestaurantRater.Controllers
         {
             return View(_db.Restaurants.ToList());
         }
+
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Restaurant/Create
+        [HttpPost, ValidateAntiForgeryToken] //Just to show this is ONLY a post because there is another create method. ValidateAntiForegeryToken-- see in the view
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");//Return to home page of Restaurant
+            }
+
+            return View(restaurant);//give model back to the view -- aka puts everything back in the form that you sent autopopulated. This would be like if you have one error and everything is still saved for you.
+        }
     }
 }
